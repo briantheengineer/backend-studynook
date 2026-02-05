b4f135f41a3b283c99428bc18c22efd5c998a545
Brian H. Castelhano <118952129+briantheengineer@users.noreply.github.com>
2026-02-05 15:32:54 -0300

    fixed getDeck route

--- src/routes/deck.routes.js
+++ src/routes/deck.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createDeck, listDecks, deleteDeck } from "../controllers/deck.controller.js";
+import { createDeck, listDecks, deleteDeck, getDeck } from "../controllers/deck.controller.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import flashcardRoutes from "./flashcard.routes.js";
 
@@ -9,7 +9,7 @@ router.post("/", authMiddleware, createDeck);
 router.get("/", authMiddleware, listDecks);
 router.delete("/:deckId", authMiddleware, deleteDeck);
 router.use("/:deckId/flashcards", flashcardRoutes);
-router.get("/decks/:deckId", authMiddleware, getDeck);
+router.get("/:deckId", authMiddleware, getDeck);
 
 
 export default router;
